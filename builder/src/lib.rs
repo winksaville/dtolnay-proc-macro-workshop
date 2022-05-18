028e9c835b8b13fc4ef5f37e3ba6fb501061d1af
Wink Saville <wink@saville.com>
2022-05-18 09:30:41 -0700

    feat: Get builder 01-parse.rs working
    I think the resources for this should include add "extra-traits" so the
    Debug trait is added to the structures. This allows dbg!, println! and
    eprintln! to be used during development.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,22 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Add syn crate, the "extra-traits" are needed for Debug traits
+    // [see](https://github.com/dtolnay/syn#optional-features)
+    //    [dependencies]
+    //    syn = { version = "1.0.95", features = ["full", "extra-traits"] }
+    let derive_input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // Why does printing to stderr always work, even with just:
+    //   cargo test
+    dbg!(&derive_input);
+    //eprintln!("Builder-derive: stderr input={:#?}", &derive_input);
+
+    // Why does printing to stdout only "work" on "rebuilds" even with:
+    //   cargo test -- --nocapture --show-output
+    //println!("Builder-derive: stdout input={:#?}", &derive_input);
+
+    TokenStream::default()
 }
