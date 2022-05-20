3000d107516b7f330c70e1a75e0161b1698f0ecb
Wink Saville <wink@saville.com>
2022-05-20 15:58:56 -0700

    refactor: Add components
    First component is parse which takes a proc_macro2::TokenStream and
    returns an ItemStruct as an Ast.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,34 +1,26 @@
 use proc_macro::TokenStream;
+
+use components::parse;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields};
-
-//use utilities::parse::parse;
-//mod analyze;
+use syn::Fields;
 
 // This is going to be patterned after the ferrous-systems
 // testing-proc-macros blog post:
 //   https://ferrous-systems.com/blog/testing-proc-macros/
-// Although right now everything is here but I'm assuming
-// I'll move it to other modules later :)
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let derive_input: DeriveInput = parse_macro_input!(input);
+    let item_struct = parse(input.into());
 
-    let named_fields = match derive_input.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => fields.named,
+    let named_fields = match item_struct.fields {
+        Fields::Named(fields) => fields.named,
         _ => panic!("this derive macro only works on structs with named fields"),
     };
 
-    let struct_ident = derive_input.ident;
+    let struct_ident = item_struct.ident;
     let builder_name = format!("{}Builder", struct_ident);
     let builder_ident = Ident::new(&builder_name, Span::call_site());
 
-    // Got this loop from this blog post; https://blog.turbo.fish/proc-macro-simple-derive/
-    // But needed to use `iter()` and `clone()` the fields as I needed two loops
     let optional_named_fields = named_fields.iter().map(|field| {
         let ident = field.ident.clone();
         let ty = field.ty.clone();
